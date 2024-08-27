2ab3ccb306f1d842c63b3289210c5f40352e9537
Herrera-Daniel <dan_michigan2012@hotmail.com>
2024-08-27 16:17:28 -0600

    cleaned up main page, need to think about what this thing will acually look like

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,11 @@
 <script>
-	import Header from './Header.svelte';
 	import '../app.css';
 </script>
 
 <div class="app">
-	<Header />
-
 	<main>
 		<slot />
 	</main>
-
-	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-	</footer>
 </div>
 
 <style>
@@ -20,6 +13,8 @@
 		display: flex;
 		flex-direction: column;
 		min-height: 100vh;
+		background-color: black;
+		color: white;
 	}
 
 	main {
@@ -31,23 +26,5 @@
 		max-width: 64rem;
 		margin: 0 auto;
 		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
 	}
 </style>
