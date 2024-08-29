889537a023da16f557b910f55b662347966aef01
Herrera-Daniel <dan_michigan2012@hotmail.com>
2024-08-28 18:29:39 -0600

    more home info

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <script>
-	import Counter from './Counter.svelte';
 </script>
 
 <svelte:head>
@@ -7,12 +6,34 @@
 	<meta name="Daniel Herrera" content="Daniel Herrera's personal site" />
 </svelte:head>
 
-<section>Show a picture of me and a basic description</section>
 <section>
-	Show services, tutoring, website buiding, consultation, basic web security lessons
+	<div class="welcome">
+		Hello and welcome, my name is Daniel Herrera. I am a Software Engineer with 5 years of
+		professional experience building enterprise level applications. I graduated in 2019 with degrees
+		in Physics with an emphasis in Astronomy, and Software Engineering.
+	</div>
 </section>
 <section>
-	Provide basic contact details, or a link to reserve a time frame to discuss services
+	Whatever your software needs, I can help. Make this part look good, lol, I don't know what that
+	means right now.
+	<ul>
+		<li>Website & Application Consultation/Design/Maintenance</li>
+	</ul>
+
+	<br />
+	I also offer tutoring services and lessons for the following subjects:
+
+	<ul>
+		<li>Programming Lessons</li>
+		<li>Programming Tutoring</li>
+		<li>Math (Fundamentals, Geometry, Trigonometry & Calculus)</li>
+		<li>Physics (All levels)</li>
+		<li>Chemistry (Fundamentals)</li>
+	</ul>
+</section>
+<section>
+	If any of these services interest you, please feel free to contact me at
+	daniel.herrera33@proton.me. Or use the <a href="/scheduler">scheduler</a> to request a meeting time.
 </section>
 
 <style>
@@ -22,5 +43,7 @@
 		justify-content: center;
 		align-items: center;
 		flex: 0.6;
+	}
+	.welcome {
 	}
 </style>
