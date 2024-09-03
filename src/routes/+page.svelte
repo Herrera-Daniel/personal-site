c08a02023fe7fafa086dd88f1e11fe56dd739142
Herrera-Daniel <dan_michigan2012@hotmail.com>
2024-09-03 16:54:45 -0600

    updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,12 +7,14 @@
 	<meta name="Daniel Herrera" content="Daniel Herrera's personal site" />
 </svelte:head>
 
+<!-- #TODO -->
 <section>
-	<div class="welcome">
-		<div class="welcomeText">
-			<h1>Hello and welcome, my name is Daniel Herrera.</h1>
-		</div>
-		<img class="welcomeImg" src={me} alt="me" />
+	<div class="welcomeContainer">
+		<h1 class="welcome">Welcome</h1>
+		<h2 class="welcomeTag">
+			<span> My name is </span>
+			<span class="welcomeName"> Daniel Herrera </span>
+		</h2>
 	</div>
 	<div class="description">
 		<h2>
@@ -20,63 +22,62 @@
 			applications. I graduated in 2019 with degrees in Physics with an emphasis in Astronomy, and
 			Software Engineering.
 		</h2>
-
-		Whatever your software needs, I can help. Make this part look good, lol, I don't know what that
-		means right now.
 	</div>
 
-	<ul>
-		<li>Website Design & Maintenance</li>
-		<li>Application Consultation, Design, Implementation, Maintenance</li>
-	</ul>
+	<div class="services">
+		<h4>
+			I offer a variety of servies, from tutoring and lessons to website development. If anything
+			interests you please contact be at daniel.herrera@proton.me or use the
+			<a href="/scheduler">scheduler</a> to request a meeting.
+		</h4>
+		<ul>
+			<li>Website Design & Maintenance</li>
+			<li>Application Consultation, Design, Implementation, Maintenance</li>
+		</ul>
 
-	<br />
-	I also offer tutoring services and lessons for the following subjects:
+		<br />
+		I also offer tutoring services and lessons for the following subjects:
 
-	<ul>
-		<li>Programming Lessons</li>
-		<li>Programming Tutoring</li>
-		<li>Math (Fundamentals, Geometry, Trigonometry & Calculus)</li>
-		<li>Physics (All levels)</li>
-		<li>Chemistry (Fundamentals)</li>
-	</ul>
-	If any of these services interest you, please feel free to contact me at daniel.herrera33@proton.me.
-	Or use the<a href="/scheduler">scheduler</a> to request a meeting time.
+		<ul>
+			<li>Programming Lessons</li>
+			<li>Programming Tutoring</li>
+			<li>Math (Fundamentals, Geometry, Trigonometry & Calculus)</li>
+			<li>Physics (All levels)</li>
+			<li>Chemistry (Fundamentals)</li>
+		</ul>
+		If any of these services interest you, please feel free to contact me at daniel.herrera33@proton.me.
+		Or use the to request a meeting time.
+	</div>
 </section>
 
 <style>
 	section {
 		display: flex;
 		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
 	}
 
 	.description {
 		display: flex;
 		flex-direction: column;
-		text-align: center;
 		margin-top: 4em;
 		width: 48em;
 	}
 
-	.welcome {
-		display: inline-flex;
-		gap: 8em;
-	}
-
-	.welcomeText {
+	.welcomeContainer {
 		display: flex;
-		justify-content: center;
-		text-align: center;
-		align-items: center;
-		width: 36em;
+		flex-direction: column;
+		width: 100%;
 	}
 
-	.welcomeImg {
-		width: 12em;
-		height: 12em;
-		border-radius: 50px;
+	.welcome {
+		font-size: 4em;
+	}
+
+	.welcomeTag {
+		font-size: 2.5em;
+	}
+
+	.welcomeName {
+		color: var(--color-theme-1);
 	}
 </style>
