876912c2d855db2d460ddf1bc8fa7bb51cba3ba6
Herrera-Daniel <dan_michigan2012@hotmail.com>
2024-09-05 21:38:24 -0600

    added basic calendar for schedule

--- src/routes/schedule/+page.svelte
+++ src/routes/schedule/+page.svelte
@@ -1,1 +1,14 @@
-<h1>This is where the schedule will go, still need to finalize design.</h1>
+<script lang="ts">
+	import Calendar from '@/components/ui/calendar/calendar.svelte';
+	import { getLocalTimeZone, today } from '@internationalized/date';
+
+	let value = today(getLocalTimeZone());
+</script>
+
+<section class="flex flex-col items-center gap-8">
+	<h1>This is where the schedule will go, still need to finalize design.</h1>
+	<div class="flex flex-col sm:flex-row border w-10/12 p-8 rounded-md gap-12">
+		<Calendar bind:value class="rounded-md border w-fit items-center" />
+		{value}
+	</div>
+</section>
