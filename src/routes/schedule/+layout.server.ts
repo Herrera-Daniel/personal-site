d3afc7afc985498a6e6cd8b433ce71648a25c9a5
tubadan33 <daniel.herrera33@proton.me>
2024-09-15 00:49:04 -0600

    various fixes

--- src/routes/schedule/+layout.server.ts
+++ src/routes/schedule/+layout.server.ts
@@ -4,10 +4,6 @@ import { JWT } from 'google-auth-library';
 import { google } from 'googleapis';
 import type { LayoutServerLoad } from './$types';
 
-export const config = {
-	runtime: 'nodejs'
-};
-
 const SCOPES = [
 	'https://www.googleapis.com/auth/calendar',
 	'https://www.googleapis.com/auth/calendar.events'
