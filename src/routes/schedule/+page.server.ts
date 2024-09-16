c601eccb3781c9bbbc5a739f85756a325b751e94
tubadan33 <daniel.herrera33@proton.me>
2024-09-15 19:19:39 -0600

    move loading to page server, no need for layout server

--- src/routes/schedule/+page.server.ts
+++ src/routes/schedule/+page.server.ts
@@ -1,8 +1,10 @@
 import {
 	DateFormatter,
+	getLocalTimeZone,
 	isSameDay,
 	parseAbsoluteToLocal,
-	parseZonedDateTime
+	parseZonedDateTime,
+	today
 } from '@internationalized/date';
 import nodemailer from 'nodemailer';
 import mg from 'nodemailer-mailgun-transport';
@@ -25,6 +27,13 @@ const SCOPES = [
 const CALENDAR_ID =
 	'840dfa8a8e3b6e75c172d138cfb3a745ed64ce752bafe00e48a1bcd1865f9dc1@group.calendar.google.com';
 
+type CalendarEvent = {
+	summary: string;
+	start: { dateTime: string; timeZone: string };
+	end: { dateTime: string; timeZone: string };
+	status: string;
+};
+
 const mailgunAuth = {
 	auth: {
 		api_key: EMAIL_API_KEY,
@@ -40,6 +49,56 @@ const calendarAuth = new JWT({
 
 const nodemailerMailgun = nodemailer.createTransport(mg(mailgunAuth));
 
+export const load: PageServerLoad = async () => {
+	const items = google
+		.calendar({ version: 'v3' })
+		.events.list({
+			auth: calendarAuth,
+			calendarId: CALENDAR_ID,
+			showDeleted: false,
+			singleEvents: true,
+			maxResults: 10,
+			timeMin: today(getLocalTimeZone()).toDate('America/Denver').toISOString(),
+			orderBy: 'startTime'
+		})
+		.then((res) => res.data)
+		.then((data) => {
+			const events = data.items as CalendarEvent[];
+
+			return events
+				.filter((i) => i.summary === 'Free')
+				.map((i) => {
+					const startDate = parseAbsoluteToLocal(i.start.dateTime);
+					const endDate = parseAbsoluteToLocal(i.end.dateTime);
+					const hours = endDate.toDate().getHours() - startDate.toDate().getHours();
+					const otherEventsOnDay = events.filter(
+						(se) =>
+							isSameDay(startDate, parseAbsoluteToLocal(se.start.dateTime)) && se.summary !== 'Free'
+					);
+
+					const times = [...Array(hours).keys()].map((t) => {
+						const hour = parseAbsoluteToLocal(i.start.dateTime).add({ hours: t });
+						const reserved = otherEventsOnDay.find(
+							(se) => parseAbsoluteToLocal(se.start.dateTime).toString() === hour.toString()
+						);
+
+						return {
+							time: hour.toString(),
+							reserved: reserved !== undefined
+						};
+					});
+					return {
+						summary: i.summary,
+						start: i.start.dateTime,
+						end: i.end.dateTime,
+						times: times.filter((t) => !t.reserved)
+					};
+				});
+		});
+
+	return { events: items };
+};
+
 export const actions = {
 	default: async ({ request }) => {
 		let error;
@@ -51,7 +110,9 @@ export const actions = {
 				from: 'mailgun@sandboxb377c6e2383f42359367d636f993f6f8.mailgun.org',
 				to: 'daniel.herrera33@proton.me',
 				subject: 'New Meeting Request',
-				text: `Date: ${data.get('date')}\nTime: ${formatTimeFromString(data.get('startTime') as string)}\nName: ${data.get('name')}\nEmail: ${data.get('email')}\nService: ${data.get('service')}`
+				text: `Date: ${data.get('date')}\nTime: ${formatTimeFromString(
+					data.get('startTime') as string
+				)}\nName: ${data.get('name')}\nEmail: ${data.get('email')}\nService: ${data.get('service')}`
 			},
 			(err) => {
 				if (err) {
